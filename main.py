34aeb724eb6f253d9d81579a04f532a6bc330a1b
Omer Aplatony <omerap12@gmail.com>
2023-01-01 18:32:14 +0200

    manage to connect to mongo atlas

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import sys
-# from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request
 import pymongo
 
-if __name__ == "__main__":
-    connection_url = f'mongodb+srv://els_admin:{sys.argv[1]}@els.r9xuzuv.mongodb.net/test'
-    # app = Flask(__name__)
-    client = pymongo.MongoClient(connection_url)
-    collection = client.els_db.els_db
+app = Flask(__name__)
 
-    # get all users
-    users = collection.find_one()
-    print(users)
+@app.route("/home")
+def home():
+    return "Hello, Flask!"
+
+if __name__ == "__main__":
+    # connection_url = f'mongodb+srv://els_admin:{sys.argv[1]}@els.r9xuzuv.mongodb.net/test'
+    # # app = Flask(__name__)
+    # client = pymongo.MongoClient(connection_url)
+    # collection = client.els_db.els_db
+    #
+    # # get all users
+    # users = collection.find_one()
+    # print(users)
+    app.run()
+
 
