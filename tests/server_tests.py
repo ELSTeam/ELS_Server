b729e157a26399d9b8bfcd05450ada460a16e1ab
Omer <61663422+omerap12@users.noreply.github.com>
2023-01-02 18:21:39 +0200

    Test file and delete method changed
    * changed the delete in server to DELETE and add delete test in testing file

--- tests/server_tests.py
+++ tests/server_tests.py
@@ -21,14 +21,39 @@ def check_sign_in(url):
         payload["username"] = pair[0]
         payload["password"] = pair[1]
         res = requests.post(url, json=payload, headers=headers)
-        # Todo - check this assert
         assert res.status_code == 400
 
     # positive test - user does exist
     payload["username"] = "omerap12"
     payload["password"] = "Aa123456!"
     res = requests.post(url, json=payload, headers=headers)
-    # Todo - check this assert
+    assert res.status_code == 200
+
+
+def sign_up(url):
+    """
+    Function purpose - check if the sign up of function works in server side at /sign_up
+    """
+    # user already exist
+    payload = {"username": "omerap12", "password": "password"}
+    headers = {'Content-Type': 'application/json'}
+    res = requests.post(url, json=payload, headers=headers)
+    assert res.status_code == 400
+
+    # user sign up successfully
+    payload["username"] = "test_user"
+    payload["password"] = "test_password"
+    res = requests.post(url, json=payload, headers=headers)
+    assert res.status_code == 200
+
+
+def delete_user(url):
+    """
+    Function purpose - check for deleting user in server side at /delete
+    """
+    payload = {"username": "test_user", "password": "test_password"}
+    headers = {'Content-Type': 'application/json'}
+    res = requests.delete(url, json=payload, headers=headers)
     assert res.status_code == 200
 
 
@@ -36,4 +61,14 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python3 server_test.py [url]")
         exit(1)
-    check_sign_in(sys.argv[1]+'/sign_in')
+    print("Checking sign in")
+    check_sign_in(sys.argv[1] + '/sign_in')
+    print("Passed")
+
+    print("Checking sign up")
+    sign_up(sys.argv[1] + '/sign_up')
+    print("Passed")
+
+    print("Checking delete user")
+    delete_user(sys.argv[1] + '/delete')
+    print("Passed")
