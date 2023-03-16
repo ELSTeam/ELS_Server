5d6ca26e3c3c08a9fa3ceb3d498ff0cce2841153
Omer Aplatony <omerap12@gmail.com>
2023-03-16 21:03:47 +0200

    Added email sender object file

---
+++ email_sender.py
@@ -0,0 +1,24 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+class EmailSender:
+    def __init__(self) -> None:
+        # Define email sender and receiver
+        self.email_sender = 'elderlylifesavior@gmail.com'
+        self.email_password = 'ofocdoeyyvpblrka'
+    def send_mail(self,email_receiver:str, subject:str,message:str) -> None:
+        """
+        Send to email_receiver an email with subject and message
+        """
+        em = EmailMessage()
+        em['From'] = self.email_sender
+        em['To'] = email_receiver
+        em['Subject'] = subject
+        em.set_content(message)
+        # Add SSL (layer of security)
+        context = ssl.create_default_context()
+        # Log in and send the email
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(self.email_sender, self.email_password)
+            smtp.sendmail(self.email_sender, email_receiver, em.as_string())
