015490c8750b5c9cfd0c3692209014572f00a650
AvitalAviv <avitalos6@gmail.com>
2023-03-24 14:12:29 +0300

    fixed

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
             for contact in contacts_list:
                 phone = contact['phone']
                 email = contact['email']
-                email_sender.send_mail(email,"Fall detected",f'KUDOS!\nClick here to confirm: http://127.0.0.1:5000/fall_detected/{user_name}')
+                email_sender.send_mail(email, "Fall detected",f'KUDOS!\nClick here to confirm: http://127.0.0.1:5000/fall_detected/{username}')
                 # sms_sender.send_message(phone,f'KUDOS!\nClick here to confirm: http://127.0.0.1:5000/fall_detected/{user_name}')
                 # sms_sender.. - production
             time.sleep(TIME)
